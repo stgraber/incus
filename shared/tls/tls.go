1808b3a22fef192c003b43d789638427b2379f29
Stéphane Graber <stgraber@stgraber.org>
2025-03-28 21:12:22 -0400

    shared/tls: Make golangci-lint clean
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/tls/tls.go
+++ shared/tls/tls.go
@@ -19,7 +19,7 @@ const connectErrorPrefix = "Unable to connect to"
 
 // RFC3493Dialer connects to the specified server and returns the connection.
 // If the connection cannot be established then an error with the connectErrorPrefix is returned.
-func RFC3493Dialer(context context.Context, network string, address string) (net.Conn, error) {
+func RFC3493Dialer(_ context.Context, network string, address string) (net.Conn, error) {
 	host, port, err := net.SplitHostPort(address)
 	if err != nil {
 		return nil, err
