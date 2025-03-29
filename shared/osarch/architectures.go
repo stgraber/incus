a4559395265680b6a7d9b3b1b3d3505451ccae01
Stéphane Graber <stgraber@stgraber.org>
2025-03-28 21:16:06 -0400

    shared/osarch: Make golangci-lint clean
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- shared/osarch/architectures.go
+++ shared/osarch/architectures.go
@@ -97,25 +97,25 @@ var architectureSupportedPersonalities = map[int][]int{
 const ArchitectureDefault = "x86_64"
 
 func ArchitectureName(arch int) (string, error) {
-	arch_name, exists := architectureNames[arch]
+	archName, exists := architectureNames[arch]
 	if exists {
-		return arch_name, nil
+		return archName, nil
 	}
 
 	return "unknown", fmt.Errorf("Architecture isn't supported: %d", arch)
 }
 
 func ArchitectureId(arch string) (int, error) {
-	for arch_id, arch_name := range architectureNames {
-		if arch_name == arch {
-			return arch_id, nil
+	for archID, archName := range architectureNames {
+		if archName == arch {
+			return archID, nil
 		}
 	}
 
-	for arch_id, arch_aliases := range architectureAliases {
-		for _, arch_name := range arch_aliases {
-			if arch_name == arch {
-				return arch_id, nil
+	for archID, archAliases := range architectureAliases {
+		for _, archName := range archAliases {
+			if archName == arch {
+				return archID, nil
 			}
 		}
 	}
@@ -124,9 +124,9 @@ func ArchitectureId(arch string) (int, error) {
 }
 
 func ArchitecturePersonality(arch int) (string, error) {
-	arch_personality, exists := architecturePersonalities[arch]
+	archPersonality, exists := architecturePersonalities[arch]
 	if exists {
-		return arch_personality, nil
+		return archPersonality, nil
 	}
 
 	return "", fmt.Errorf("Architecture isn't supported: %d", arch)
